9c81ccfa5295271abe0d9eb204cbcced12f84619
youliyav <youliyav@gmail.com>
2019-01-27 21:33:38 +0300

    add new homework

---
+++ 1.1.Function2/1.1_Function 2.0 args, kwargs_.py
@@ -0,0 +1,25 @@
+def adv_print(*args, start='\n', max_line=int(), in_file=False):
+    if max_line:
+        p_list = list()
+        m = max_line
+        for a in args:
+            p_list.append(a)
+        p_list = start + ' '.join([str(a) for a in p_list])
+        counter = 0
+
+        for a in range(int(len(p_list) / m) + 1):
+            if in_file:
+                with open('file.txt', 'a', encoding='utf-8') as f:
+                    f.write(p_list[counter:max_line] + '\n')
+                    counter += m
+                    max_line += m
+            else:
+                print(p_list[counter:max_line])
+                counter += m
+                max_line += m
+
+    else:
+        print(start, *args)
+
+
+adv_print('Арозаупалана лапуАзора', 467392765, max_line=7, in_file=True)
